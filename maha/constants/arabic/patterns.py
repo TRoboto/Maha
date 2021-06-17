6984d8e7565cac548cc44e45e29f68405e06c4ea
TRoboto <m4bh@hotmail.com>
2021-06-17 23:38:40 +0300

    Add docstrings

--- maha/constants/arabic/patterns.py
+++ maha/constants/arabic/patterns.py
@@ -1,2 +1,9 @@
-PATTERN_ARABIC_HASHTAGS = r"(?<=\s|^|\*|\n)(#[0-9_أ-ي][-أ-ي_][-0-9أ-ي_]*)(?=\s| |$)"
+""" Regular expersion patterns for Arabic """
+
+PATTERN_ARABIC_HASHTAGS: str = (
+    r"(?<=\s|^|\*|\n)(#[0-9_أ-ي][-أ-ي_][-0-9أ-ي_]*)(?=\s| |$)"
+)
+""" Pattern that matches Arabic hashtags """
+
 PATTERN_ARABIC_MENTIONS = r"(?<=\s|^|\*|\n)(@[0-9_أ-ي][-أ-ي_][-0-9أ-ي_]*)(?=\s| |$)"
+""" Pattern that matches Arabic mentions """
--- maha/constants/english/patterns.py
+++ maha/constants/english/patterns.py
@@ -1,2 +1,6 @@
-PATTERN_ENGLISH_HASHTAGS = r"(?<=\s|^|\*|\n)(#\w[-a-zA-Z0-9_][-\w_]*)(?=\s| |$)"
-PATTERN_ENGLISH_MENTIONS = r"(?<=\s|^|\*|\n)(@\w[-a-zA-Z0-9_][-\w_]*)(?=\s| |$)"
+""" Regular expersion patterns for English """
+
+PATTERN_ENGLISH_HASHTAGS: str = r"(?<=\s|^|\*|\n)(#\w[-a-zA-Z0-9_][-\w_]*)(?=\s| |$)"
+""" Pattern that matches English hashtags """
+PATTERN_ENGLISH_MENTIONS: str = r"(?<=\s|^|\*|\n)(@\w[-a-zA-Z0-9_][-\w_]*)(?=\s| |$)"
+""" Pattern that matches English mentions """
