9f0a9dad2c78f158314a3b96b25f63479a8c7057
TRoboto <m4bh@hotmail.com>
2021-06-29 23:20:23 +0300

    Refactor processors

--- maha/processors/basic_processors.py
+++ maha/processors/basic_processors.py
@@ -1,7 +1,23 @@
 import pathlib
-from typing import Union
+from typing import Callable, Union
 
-from .text_processor import TextProcessor
+from .base_processor import BaseProcessor
+
+
+class TextProcessor(BaseProcessor):
+    """For processing text input.
+
+    Parameters
+    ----------
+    text : Union[List[str], str]
+        A text or list of strings to process
+    """
+
+    def apply(self, fn: Callable[[str], str]):
+        self.lines = list(map(fn, self.lines))
+
+    def filter(self, fn: Callable[[str], bool]):
+        self.lines = list(filter(fn, self.lines))
 
 
 class FileProcessor(TextProcessor):
@@ -35,3 +51,7 @@ class FileProcessor(TextProcessor):
             raise ValueError("File empty.")
 
         super().__init__(lines)
+
+
+class FolderProcessor:
+    pass
