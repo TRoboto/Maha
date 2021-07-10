ce8ab5031fbda94e46b3cdfa6e8ff1d4fdcbb30a
TRoboto <m4bh@hotmail.com>
2021-07-10 17:56:27 +0300

    Move :class:`~.ObjectGet`

---
+++ maha/processors/utils.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from functools import wraps
+from typing import Any, Callable
+
+
+@dataclass
+class ObjectGet:
+    """Used with get function in :class:`BaseProcessor`"""
+
+    # function to use
+    func: Callable
+    # initial value
+    prev: Any
+    # name of the operation (argument name)
+    name: str
+    # Function to apply at end
+    # Defaults for post_fn, return the input
+    post_fn: Callable = lambda input: input
