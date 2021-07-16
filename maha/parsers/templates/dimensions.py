eecc74a2bcc8ae8b75e58a69c4e8f705b6776ee0
TRoboto <m4bh@hotmail.com>
2021-07-16 19:03:06 +0300

    Improve dimensions

--- maha/parsers/templates/dimensions.py
+++ maha/parsers/templates/dimensions.py
@@ -1,9 +1,41 @@
-__all__ = ["Dimension", "UnitDimension", "DimensionPattern", "UnitDimensionPattern"]
+__all__ = ["Dimension", "Expression"]
 
-from dataclasses import dataclass
-from typing import List, NamedTuple, Union
+from dataclasses import dataclass, field
+from typing import Optional
 
 from .types import DimensionType, Unit
+
+
+@dataclass
+class Expression:
+    __slots__ = ["pattern", "is_confident", "output"]
+
+    pattern: str
+    """Regular expersion(s) to match"""
+    is_confident: bool
+    """Whether the extracted value 100% belongs to the selected dimension. Some patterns
+    may match for values that normally belong to the dimension but not always."""
+    output: Optional[str]
+    """
+    Whether a simple exp or captured groups (\1\2...). Expressions with captured groups
+    are also supported. For instance, the following pattern: ``\1 + \2`` will sum up
+    the values of the captured groups.
+
+    When ``output`` is set to ``None``, the value is extracted from text
+    """
+
+    def __init__(
+        self, pattern: str, is_confident: bool = False, output: Optional[str] = None
+    ):
+        self.pattern = pattern
+        self.is_confident = is_confident
+        self.output = output
+
+    def __repr__(self):
+        out = f"Expression(pattern={self.pattern}, is_confident={self.is_confident}"
+        if self.output:
+            return out + f", output={self.output})"
+        return out + ")"
 
 
 @dataclass
@@ -11,63 +43,49 @@ class Dimension:
     """Template for the parsed item"""
 
     __slots__ = [
-        "expression",
+        "matched_expression",
         "value",
         "start",
         "end",
-        "is_confident",
-        "dimension",
+        "dimension_type",
+        "unit",
     ]
 
-    expression: Union[List[str], str]
-    """Regular expersion(s) to match"""
+    matched_expression: Expression
+    """Expression(s) to match"""
     value: str
     """Extracted value"""
     start: int
     """Start index of the value in the text"""
     end: int
     """End index of the value in the text"""
-    is_confident: bool
-    """Whether the extracted value 100% belongs to the selected dimension. Some patterns
-    may match for values that normally belong to the dimension but not always."""
-    dimension: DimensionType
+    dimension_type: DimensionType
     """Dimension type."""
-
-
-@dataclass
-class UnitDimension(Dimension):
-    """Dimension with unit"""
-
-    __slots__ = ["unit"]
-    unit: Unit
+    unit: Optional[Unit]
     """Unit of the dimension"""
 
+    def __init__(
+        self,
+        matched_expression: Expression,
+        value: str,
+        start: int,
+        end: int,
+        dimension_type: DimensionType,
+        unit: Optional[Unit] = None,
+    ):
+        self.matched_expression = matched_expression
+        self.value = value
+        self.start = start
+        self.end = end
+        self.dimension_type = dimension_type
+        self.unit = unit
 
-@dataclass(init=False)
-class DimensionPattern(Dimension):
-    """Dimension with pattern"""
-
-    __slots__ = ["output"]
-
-    output: str
-    """
-    Whether a simple exp or captured groups (\1\2...). Expressions with captured groups
-    are also supported. For instance, the following pattern: ``\1 + \2`` will sum up
-    the values of the captured groups.
-    """
-
-    def __init__(self):
-        pass
-
-    @classmethod
-    def copy(cls):
-        return cls()
-
-
-@dataclass(init=False)
-class UnitDimensionPattern(DimensionPattern):
-    """Dimension with unit"""
-
-    __slots__ = ["unit"]
-    unit: Unit
-    """Unit of the dimension"""
+    def __repr__(self):
+        out = (
+            f"Dimension(matched_expression={self.matched_expression}, "
+            f"value={self.value}, start={self.start}, end={self.end}, "
+            f"dimension_type={self.dimension_type}"
+        )
+        if self.unit:
+            return out + f", unit={self.unit})"
+        return out + ")"
