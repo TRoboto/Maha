3e58e90382e83fd8e13337a6f27e0064525a19f0
TRoboto <m4bh@hotmail.com>
2021-07-20 01:36:51 +0300

    Add more functions

--- maha/parsers/expressions/helper.py
+++ maha/parsers/expressions/helper.py
@@ -1,9 +1,32 @@
 """
 Functions that return shared expressions.
 """
+from maha.constants import ARABIC_NUMBERS, ENGLISH_NUMBERS
 
-NUMBER_EXPRESSION: str = "([0-9]+)"
+INTEGER_EXPRESSION: str = "([{}]+)".format("".join(ARABIC_NUMBERS + ENGLISH_NUMBERS))
+DECIMAL_EXPRESSION: str = "([{}].[{}]+)".format(
+    "".join(ARABIC_NUMBERS + ENGLISH_NUMBERS)
+)
+SPACE_EXPRESSION: str = r"\s+"
 
 
 def get_number_followed_by_string(expression: str) -> str:
-    return r"\b{}\s*{}\b".format(NUMBER_EXPRESSION, expression)
+    return get_words_separated_by_space(INTEGER_EXPRESSION, expression)
+
+
+def get_decimal_followed_by_string(expression: str) -> str:
+    return get_words_separated_by_space(DECIMAL_EXPRESSION, expression)
+
+
+def get_words_separated_by_space(*words: str):
+    """
+    Returns a regex that matches words separated by spaces.
+    """
+    return get_word(SPACE_EXPRESSION.join(words))
+
+
+def get_word(word: str) -> str:
+    """
+    Returns a regex that matches a complete word.
+    """
+    return r"\b{}\b".format(word)
