b1f54ba7959b0a2bc82216705f18f2f61c46a52c
TRoboto <m4bh@hotmail.com>
2021-07-23 01:27:35 +0300

    Add simple tests for minutes

--- tests/parsers/expressions/test_duration.py
+++ tests/parsers/expressions/test_duration.py
@@ -23,6 +23,56 @@ from maha.parsers.templates.types import DurationUnit
 @pytest.mark.parametrize(
     "expected, input",
     [
+        (1, "دقيقة"),
+        (1, "دقيقه"),
+        (2, "دقيقتين"),
+        (2, "ودقيقتان"),
+        (4, "4 دقايق"),
+        (30, "30 دقيقة"),
+        (3, "3 دقائق"),
+        (4.5, "4 دقائق ونص"),
+        (3.25, "3 دقيقه و ربع"),
+        (4.5, "4.5 دقيقة"),
+        (13 + 1 / 3, "13 دقيقه وثلث"),
+    ],
+)
+def test_parse_minutes_with_simple_values(input: str, expected: float):
+    output = list(EXPRESSION_DURATION_MINUTES.parse(input))
+    assert len(output) == 1
+    output = output[0]
+
+    assert output.value == expected
+    assert output.expression.unit == DurationUnit.MINUTES
+
+
+@pytest.mark.parametrize(
+    "expected, input",
+    [
+        (135, "دقيقتين وربع"),
+        (140, "دقيقتان و ثلث"),
+        (150, "ودقيقتين ونصف"),
+        (150, "دقيقتين ونص"),
+        (90, "دقيقه ونصف"),
+        (75, "دقيقه وربع"),
+        (80, "دقيقة وثلث"),
+        (45, "دقيقة الا ربع"),
+        (15, "ربع دقيقه"),
+        (30, "نص دقيقه"),
+        (20, "ثلث دقيقه"),
+    ],
+)
+def test_parse_minutes_with_more_simple_values(expected: float, input: str):
+    output = list(EXPRESSION_DURATION_MINUTES.parse(input))
+    assert len(output) == 1
+    output = output[0]
+
+    assert output.value == expected
+    assert output.expression.unit == DurationUnit.SECONDS
+
+
+@pytest.mark.parametrize(
+    "expected, input",
+    [
         (1, "ساعة"),
         (1, "ساعه"),
         (2, "ساعتين"),
@@ -32,7 +82,9 @@ from maha.parsers.templates.types import DurationUnit
         (30, "30 ساعه"),
         (3, "3 ساعات"),
         (4.5, "4 ساعات ونص"),
+        (3.25, "3 ساعات وربع"),
         (4.5, "4.5 ساعة"),
+        (13 + 1 / 3, "13 ساعة وثلث"),
     ],
 )
 def test_parse_hours_with_simple_values(input: str, expected: float):
@@ -50,6 +102,7 @@ def test_parse_hours_with_simple_values(input: str, expected: float):
         (135, "ساعتين وربع"),
         (140, "ساعتان و ثلث"),
         (150, "ساعتين ونص"),
+        (150, "وساعتان ونصف"),
         (90, "ساعة ونص"),
         (75, "ساعة وربع"),
         (80, "ساعة وثلث"),
