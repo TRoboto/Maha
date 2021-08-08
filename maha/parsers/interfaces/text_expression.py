15fa6691e7cf68a1181adef3387f119f370381cf
Mohammad Al-Fetyani <m4bh@hotmail.com>
2021-08-08 03:07:16 +0300

    Add simple docstring

--- maha/parsers/interfaces/text_expression.py
+++ maha/parsers/interfaces/text_expression.py
@@ -7,6 +7,10 @@ from .expression_result import ExpressionResult
 
 
 class TextExpression(Expression):
+    """
+    Expression that returns the matched text as value
+    """
+
     def parse(self, match: Match, text: str) -> "ExpressionResult":
         return ExpressionResult(
             match.start(), match.end(), text[match.start() : match.end()], self
