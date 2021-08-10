1165aa91207ea28818dbb56fd3ffb6d6f247c426
Mohammad Al-Fetyani <m4bh@hotmail.com>
2021-08-10 21:14:08 +0300

    Apply changes to Arabic and English patterns as well

--- maha/constants/arabic/patterns.py
+++ maha/constants/arabic/patterns.py
@@ -1,9 +1,26 @@
 """ Regular expersion patterns for Arabic """
 
-PATTERN_ARABIC_HASHTAGS: str = (
-    r"(?<=\s|^|\*|\n)(#[0-9_أ-ي][-أ-ي_][-0-9أ-ي_]*)(?=\s| |$)"
+import re
+
+from ..english import AND_SIGN, AT_SIGN, ENGLISH_NUMBERS, HASHTAG, UNDERSCORE
+from ..general import PUNCTUATIONS
+from .compound import ALL_HARAKAT, ARABIC_LETTERS, ARABIC_NUMBERS
+from .simple import TATWEEL
+
+PATTERN_ARABIC_HASHTAGS: str = r"(?<=\s|^|\n|{})(#(?:[{}_][-{}]?)+)\b".format(
+    "|".join(
+        [
+            re.escape(pun)
+            for pun in PUNCTUATIONS
+            if pun not in [AT_SIGN, AND_SIGN, UNDERSCORE]
+        ]
+    ),
+    "".join(ARABIC_LETTERS + ALL_HARAKAT + ARABIC_NUMBERS) + TATWEEL,
+    "".join(ENGLISH_NUMBERS),
 )
 """ Pattern that matches Arabic hashtags """
 
-PATTERN_ARABIC_MENTIONS = r"(?<=\s|^|\*|\n)(@[0-9_أ-ي][-أ-ي_][-0-9أ-ي_]*)(?=\s| |$)"
+PATTERN_ARABIC_MENTIONS = PATTERN_ARABIC_HASHTAGS.replace(AT_SIGN, HASHTAG).replace(
+    HASHTAG, AT_SIGN
+)
 """ Pattern that matches Arabic mentions """
--- maha/constants/english/patterns.py
+++ maha/constants/english/patterns.py
@@ -1,6 +1,23 @@
 """ Regular expersion patterns for English """
 
-PATTERN_ENGLISH_HASHTAGS: str = r"(?<=\s|^|\*|\n)(#\w[-a-zA-Z0-9_][-\w_]*)(?=\s| |$)"
+import re
+
+from ..general import PUNCTUATIONS
+from .compound import ENGLISH_LETTERS, ENGLISH_NUMBERS
+from .simple import AND_SIGN, AT_SIGN, HASHTAG, UNDERSCORE
+
+PATTERN_ENGLISH_HASHTAGS: str = r"(?<=\s|^|\n|{})(#(?:[{}_]-?)+)\b".format(
+    "|".join(
+        [
+            re.escape(pun)
+            for pun in PUNCTUATIONS
+            if pun not in [AT_SIGN, AND_SIGN, UNDERSCORE]
+        ]
+    ),
+    "".join(ENGLISH_LETTERS + ENGLISH_NUMBERS),
+)
 """ Pattern that matches English hashtags """
-PATTERN_ENGLISH_MENTIONS: str = r"(?<=\s|^|\*|\n)(@\w[-a-zA-Z0-9_][-\w_]*)(?=\s| |$)"
+PATTERN_ENGLISH_MENTIONS: str = PATTERN_ENGLISH_HASHTAGS.replace(
+    AT_SIGN, HASHTAG
+).replace(HASHTAG, AT_SIGN)
 """ Pattern that matches English mentions """
