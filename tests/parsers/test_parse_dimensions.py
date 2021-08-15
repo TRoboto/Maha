205367b93174b5294187b070e70617b6db3fca06
Mohammad Al-Fetyani <m4bh@hotmail.com>
2021-08-15 03:42:08 +0300

    Fix test

--- tests/parsers/test_parse_dimensions.py
+++ tests/parsers/test_parse_dimensions.py
@@ -5,4 +5,4 @@ def test_parse_numeral_wiki(wiki_arlang):
     numerals = parse_dimension(wiki_arlang, numeral=True)
     assert len(numerals) == 12
     assert numerals[0].value == 467000000
-    assert numerals[-1].value == 10
+    assert numerals[-1].value == 29
