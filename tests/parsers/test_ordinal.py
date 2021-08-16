8819cc896a0246d41b5a6bfb4d082a70c4fa3c11
Mohammad Al-Fetyani <m4bh@hotmail.com>
2021-08-17 02:25:43 +0300

    Add more tests

--- tests/parsers/test_ordinal.py
+++ tests/parsers/test_ordinal.py
@@ -172,141 +172,41 @@ def test_perfect_hundreds(input, expected):
     assert_expression_output(parse_dimension(input, ordinal=True), expected)
 
 
-# @pytest.mark.parametrize(
-#     "input, expected",
-#     chain(
-#         get_value(191, ["مية وواحد وتسعين"]),
-#         get_value(124, ["مائة واربعة وعشرين"]),
-#         get_value(215, ["مئتين وخمسه عشر"]),
-#         get_value(306, ["ثلاثمية وستة"]),
-#         get_value(410, ["اربعمية وعشرة"]),
-#         get_value(520, ["خمس مائة وعشرين"]),
-#         get_value(606, ["ستمية وستة"]),
-#         get_value(780, ["سبعة مائة وثمانون"]),
-#         get_value(877, ["ثمنمية وسبعة وسبعين"]),
-#         get_value(999, ["تسع مية وتسعه وتسعين"]),
-#     ),
-# )
-# def test_hundreds(input, expected):
-#     assert_expression_output(parse_dimension(input, ordinal=True), expected)
+@pytest.mark.parametrize(
+    "input, expected",
+    chain(
+        get_value(191, ["المية والواحد والتسعين"]),
+        get_value(124, ["المائة والرابع والعشرين"]),
+        get_value(215, ["المئتين والخامس عشر"]),
+        get_value(306, ["الثلاثمية والسادس"]),
+        get_value(410, ["الاربعمية والعاشر"]),
+        get_value(520, ["الخمس مائة والعشرين"]),
+        get_value(606, ["الستمية والسادس"]),
+        get_value(780, ["السبعة مائة والثمانون"]),
+        get_value(877, ["الثمنمية والسابع والسبعين"]),
+        get_value(999, ["التسع مية والتاسع والتسعين"]),
+    ),
+)
+def test_hundreds(input, expected):
+    assert_expression_output(parse_dimension(input, ordinal=True), expected)
 
 
-# @pytest.mark.parametrize(
-#     "input, expected",
-#     chain(
-#         get_value(1000, ["الف", "ألف"]),
-#         get_value(2000, ["ألفين", "الفين", "ألفان"]),
-#         get_value(3000, ["ثلاث آلاف", "ثلاثة الاف"]),
-#         get_value(4000, ["اربع الاف", "أربعة الوف"]),
-#         get_value(5000, ["خمس آلاف", "خمسة ألاف"]),
-#         get_value(6000, ["ست آلاف", "ستة آلاف"]),
-#         get_value(7000, ["سبع آلاف", "سبعة الاف"]),
-#         get_value(8000, ["ثماني آلاف", "ثمانية الاف"]),
-#         get_value(9000, ["تسع آلاف", "تسعة الاف"]),
-#     ),
-# )
-# def test_perfect_thousands(input, expected):
-#     assert_expression_output(parse_dimension(input, ordinal=True), expected)
-
-
-# @pytest.mark.parametrize(
-#     "input, expected",
-#     chain(
-#         get_value(20000, ["عشرين الف"]),
-#         get_value(1200, ["الف ومئتين"]),
-#         get_value(3220, ["ثلاثة الاف ومئتان وعشرين"]),
-#         get_value(40010, ["أربعين الف وعشرة"]),
-#         get_value(55325, ["خمسة وخمسين الف وثلاثمية وخمسة وعشرين"]),
-#         get_value(22222, ["اثنين وعشرين الف ومئتين واثنين وعشرين"]),
-#         get_value(10465, ["عشرة الاف واربعمئة وخمسة وستين"]),
-#         get_value(1030, ["الف وثلاثين"]),
-#         get_value(90001, ["تسعين الف وواحد"]),
-#         get_value(17100, ["سبعطعشر الف ومية"]),
-#     ),
-# )
-# def test_thousands(input, expected):
-#     assert_expression_output(parse_dimension(input, ordinal=True), expected)
-
-
-# @pytest.mark.parametrize(
-#     "input, expected",
-#     [
-#         ("10,000.0", 10000),
-#         ("١٠٬٠٠٠٫٠٠٠", 10000),
-#         ("-10,000", -10000),
-#         ("1٬000 000", 1000000),
-#         ("1 000 000", 1000000),
-#         ("-١٬٠٠٠٬٠٠٠", -1000000),
-#         ("+١ ٠٠٠ ٠٠٠", +1000000),
-#         ("١٬٠٠٠٬٠٠٠", 1000000),
-#         (".1", 0.1),
-#         ("٫٠١", 0.01),
-#         ("١%", 0.01),
-#         ("0.01%", 0.0001),
-#         (".01%", 0.0001),
-#         ("20.0%", 0.2),
-#         ("200%", 2),
-#     ],
-# )
-# def test_numbers(input, expected):
-#     assert_expression_output(parse_dimension(input, ordinal=True), expected)
-
-
-# @pytest.mark.parametrize(
-#     "expected, input",
-#     [
-#         (0.1, "صفر فاصلة عشرة"),
-#         (0.1, "صفر فاصلة واحد"),
-#         (3.4, "ثلاث فاصل اربع"),
-#         (10.7, "عشرة فاصلة سبعة"),
-#         (10.7, "10 فاصلة سبعة"),
-#         (10.5, "10 فاصله 5"),
-#         (33.3, "ثلاثة وثلاثين فاصلة ثلاثة"),
-#         (100 / 3, "ثلث مية"),
-#     ],
-# )
-# def test_fasila_numbers(input, expected):
-#     assert_expression_output(parse_dimension(input, ordinal=True), expected)
-
-
-# @pytest.mark.parametrize(
-#     "input, expected",
-#     chain(
-#         get_value(1000000, ["مليون"]),
-#         get_value(2000000, ["مليونين", "مليونان"]),
-#         get_value(3000000, ["ثلاث ملايين", "ثلاثة مليون"]),
-#         get_value(4000000, ["اربع ملايين", "أربعة مليون"]),
-#         get_value(5000000, ["خمس ملايين", "خمسة مليون"]),
-#         get_value(6000000, ["ست ملايين", "ستة ملايين"]),
-#         get_value(7000000, ["سبع ملايين", "سبعة ملايين"]),
-#         get_value(8000000, ["ثماني ملايين", "ثمانية ملايين"]),
-#         get_value(9000000, ["تسع ملايين", "تسعة ملايين"]),
-#     ),
-# )
-# def test_perfect_millions(input, expected):
-#     assert_expression_output(parse_dimension(input, ordinal=True), expected)
-
-
-# @pytest.mark.parametrize(
-#     "input, expected",
-#     chain(
-#         get_value(20000000, ["عشرين مليون"]),
-#         get_value(1001200, ["مليون والف ومئتين"]),
-#         get_value(3022000, ["ثلاثة مليون واثنين وعشرين الف"]),
-#         get_value(40000010, ["أربعين مليون وعشرة"]),
-#         get_value(
-#             55007325,
-#             ["خمسة وخمسين مليون وسبع الاف وثلاثمية وخمسة وعشرين"],
-#         ),
-#         get_value(2022222, ["مليونين واثنين وعشرين الف ومئتين واثنين وعشرين"]),
-#         get_value(10010465, ["عشرة مليون وعشر الاف واربعمئة وخمسة وستين"]),
-#         get_value(3001030, ["ثلاث ملايين والف وثلاثين"]),
-#         get_value(90090001, ["تسعين مليون وتسعين الف وواحد"]),
-#         get_value(17000100, ["سبعطعشر مليون ومية"]),
-#     ),
-# )
-# def test_millions(input, expected):
-#     assert_expression_output(parse_dimension(input, ordinal=True), expected)
+@pytest.mark.parametrize(
+    "input, expected",
+    chain(
+        get_value(1000, ["الألف"]),
+        get_value(1000000, ["المليون"]),
+        get_value(1000000000, ["البليون", "المليار"]),
+        get_value(1000000000000, ["التريليون", "الترليون"]),
+        get_value(1100, ["الألف والمئة"]),
+        get_value(1216, ["الألف والمئتين والسادس عشر"]),
+        get_value(1000300, ["المليون والثلاثمئة"]),
+        get_value(1000018, ["المليون والثامن عشر"]),
+        get_value(1000000020, ["البليون والعشرين", "المليار والعشرون"]),
+    ),
+)
+def test_perfect_millions(input, expected):
+    assert_expression_output(parse_dimension(input, ordinal=True), expected)
 
 
 @pytest.mark.parametrize(
