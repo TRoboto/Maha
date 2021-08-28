7503e903cc57e4dbe28e90f4d7dba90d026260d7
TRoboto <m4bh@hotmail.com>
2021-08-28 21:33:57 +0300

    Allow `Expression` to be passed

--- maha/rexy/rexy.py
+++ maha/rexy/rexy.py
@@ -8,21 +8,31 @@ __all__ = [
 ]
 
 
-def non_capturing_group(*patterns: str):
+from typing import Union
+
+from maha.rexy.templates import Expression
+
+
+def non_capturing_group(*patterns: Union[Expression, str]):
     """Returns a non capturing groups of patterns."""
     return "(?:{})".format("|".join(str(p) for p in patterns))
 
 
-def positive_lookbehind(*patterns: str):
+def positive_lookbehind(*patterns: Union[Expression, str]):
     """Returns a positive lookbehind pattern."""
     return "(?<={})".format("|".join(str(p) for p in patterns))
 
 
-def positive_lookahead(*patterns: str):
+def positive_lookahead(*patterns: Union[Expression, str]):
     """Returns positive lookahead pattern"""
     return "(?={})".format("|".join(str(p) for p in patterns))
 
 
-def named_group(name: str, pattern: str):
+def named_group(name: str, pattern: Union[Expression, str]):
     """Returns named pattern group"""
     return f"(?P<{name}>{pattern})"
+
+
+def capture_group(pattern: Union[Expression, str]):
+    """Returns a capturing group pattern"""
+    return f"({pattern})"
