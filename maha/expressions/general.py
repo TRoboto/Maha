707cb616bbd5703562a5289adb0d578e5584d30b
saedx1 <saadmtsa@gmail.com>
2021-09-19 22:03:59 -0400

    exclude arabic ligature from emojis pattern

--- maha/expressions/general.py
+++ maha/expressions/general.py
@@ -105,7 +105,6 @@ EXPRESSION_EMAILS = Expression(r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+
 
 # Adopted from https://gist.github.com/slowkow/7a7f61f495e3dbb7e3d767f97bd7304b
 # TODO: Validate that all of these are valid emojis
-# TODO: Remove Arabic Ligatures from the list
 EXPRESSION_EMOJIS = Expression(
     "["
     "\U0001F600-\U0001F64F"  # emoticons
