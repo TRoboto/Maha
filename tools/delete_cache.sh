ad42e1972fce56d9a993256c5a6da88c45e17de2
Mohammad Al-Fetyani <m4bh@hotmail.com>
2021-10-08 21:16:57 +0300

    Remove only auto generated cache

--- tools/delete_cache.sh
+++ tools/delete_cache.sh
@@ -1,5 +1,5 @@
 # Remove old rule cache files
 # This scipt should be run before commiting modified cache files to prevent repo bloat
 # Should be run from the root of the repository
-git rm --cached maha/rexy/cache/*
-rm maha/rexy/cache/*
+git rm --cached maha/rexy/cache/[0-9]*
+rm maha/rexy/cache/[0-9]*
