04fd10cdf20c6cbaecfebd7711714eb09401668d
Mohammad Al-Fetyani <m4bh@hotmail.com>
2021-11-05 17:52:39 +0300

    Merge branch main

--- maha/parsers/rules/duration/rule.py
+++ maha/parsers/rules/duration/rule.py
@@ -12,11 +12,13 @@ __all__ = [
     "parse_duration",
 ]
 
-from typing import List
+from collections import defaultdict
+from dataclasses import dataclass
+from typing import Dict, List
 
-from maha.parsers.rules.numeral.rule import RULE_NUMERAL
+from maha.parsers.rules.numeral.rule import RULE_NUMERAL, parse_numeral
 from maha.parsers.templates import FunctionValue
-from maha.rexy import named_group, non_capturing_group
+from maha.rexy import ExpressionGroup, named_group, non_capturing_group
 
 from ..common import (
     FRACTIONS,
@@ -28,25 +30,39 @@ from .template import *
 from .values import *
 
 
-def get_pattern(name: str):
+def get_pattern(singular_frac_group, singular, dual, all_units):
     """Get regex pattern for duration."""
-    singular = globals()["ONE_" + name[:-1].upper()]
-    dual = globals()["TWO_" + name.upper()]
-    plural = globals()["SEVERAL_" + name.upper()]
 
     return non_capturing_group(
-        spaced_patterns(RULE_NUMERAL, non_capturing_group(singular, plural)),
-        get_fractions_of_unit_pattern(singular),
+        spaced_patterns(RULE_NUMERAL, all_units),
+        named_group("fractions", get_fractions_of_unit_pattern(singular_frac_group)),
         dual,
         singular,
     )
 
 
-def merge_values(values: List[ValueUnit]) -> ValueUnit:
-    """Merge the values from the input ``values``."""
-    unit = values[0].unit
-    assert all(value.unit == unit for value in values)
-    return ValueUnit(sum(value.value for value in values), unit)
+def merge_same_units(values: List[ValueUnit]) -> List[ValueUnit]:
+    """Merge values with same units from the input ``values``."""
+    newvalues = {}
+    for value in values:
+        unit = value.unit
+        if unit in newvalues:
+            newvalues[unit].value += value.value
+        else:
+            newvalues[unit] = value
+    return list(newvalues.values())
+
+
+def get_unit_fraction_value(matched_text):
+    val1, val2 = matched_text.split(" ", 1)
+    fraction = FRACTIONS.get_matched_expression(val1)
+    if not fraction:
+        fraction = FRACTIONS.get_matched_expression(val2)
+        value = get_matched_value(val1)
+    else:
+        value = get_matched_value(val2)
+    value.value = fraction.value  # type: ignore
+    return value
 
 
 def _parse(matched_text, singular=None, plural=None):
@@ -64,87 +80,234 @@ def _parse(matched_text, singular=None, plural=None):
     return value
 
 
-def get_combined_value(groups, unit) -> ValueUnit:
+UnitsGroup = ExpressionGroup(
+    SEVERAL_SECONDS,
+    SEVERAL_MINUTES,
+    SEVERAL_HOURS,
+    SEVERAL_DAYS,
+    SEVERAL_WEEKS,
+    SEVERAL_MONTHS,
+    SEVERAL_YEARS,
+    TWO_SECONDS,
+    TWO_MINUTES,
+    TWO_HOURS,
+    TWO_DAYS,
+    TWO_WEEKS,
+    TWO_MONTHS,
+    TWO_YEARS,
+    ONE_SECOND,
+    ONE_MINUTE,
+    ONE_HOUR,
+    ONE_DAY,
+    ONE_WEEK,
+    ONE_MONTH,
+    ONE_YEAR,
+)
 
-    singular = globals()["ONE_" + unit[:-1].upper()]
-    dual = globals()["TWO_" + unit.upper()]
-    plural = globals()["SEVERAL_" + unit.upper()]
-    values = []
-    for group in groups:
-        unit = get_matched_value(group, singular, dual)
-        if unit:
-            values.append(unit)
-        else:
-            values.append(_parse(group, singular, plural))
-    return merge_values(values)
+
+def get_matched_value(matched_text) -> ValueUnit:
+    exp_val = UnitsGroup.get_matched_expression(matched_text).value  # type: ignore
+    return ValueUnit(value=exp_val.value, unit=exp_val.unit)
 
 
-def get_matched_value(matched_text, singular, dual):
-    if not (dual or singular):
-        return
-    if dual.fullmatch(matched_text):
-        return dual.value
-    if singular.fullmatch(matched_text):
-        return singular.value
+@dataclass
+class DurationParsedValue:
+    name: str
+    text: str
+    index: int
+    values: Dict[str, List]
+
+    def __init__(self, name, text, index) -> None:
+        self.name = name
+        self.text = text
+        self.index = index
+        self.values = defaultdict(list)
+
+    def capturesdict(self):
+        return self.values
+
+
+class IndexPriorityList:
+    def __init__(self):
+        self.items: List[DurationParsedValue] = []
+
+    def add_item(self, name, text, index):
+        self.items.append(DurationParsedValue(name, text, index))
+        self.items.sort(key=lambda i: i.index)
+
+    def add_value(self, name, value, index) -> None:
+        for item in self.items:
+            if index < item.index:
+                item.values[name].append(value)
+                break
 
 
 def parse_duration(match):
     """Parse duration."""
     groups = match.capturesdict()
-    _seconds = groups.get("seconds")
-    _minutes = groups.get("minutes")
-    _hours = groups.get("hours")
-    _days = groups.get("days")
-    _weeks = groups.get("weeks")
-    _months = groups.get("months")
-    _years = groups.get("years")
+    groups_keys = list(groups)
+    collection = IndexPriorityList()
+    for key in ["seconds", "minutes", "hours", "days", "weeks", "months", "years"]:
+        if key not in groups:
+            continue
+        for i, t in enumerate(match.starts(groups_keys.index(key) + 1)):
+            collection.add_item(key, groups[key][i], t)
 
-    value = []
-    if _years:
-        value.append(get_combined_value(_years, "years"))
-    if _months:
-        value.append(get_combined_value(_months, "months"))
-    if _weeks:
-        value.append(get_combined_value(_weeks, "weeks"))
-    if _days:
-        value.append(get_combined_value(_days, "days"))
-    if _hours:
-        value.append(get_combined_value(_hours, "hours"))
-    if _minutes:
-        value.append(get_combined_value(_minutes, "minutes"))
-    if _seconds:
-        value.append(get_combined_value(_seconds, "seconds"))
+    for key in [
+        "trillions",
+        "billions",
+        "millions",
+        "thousands",
+        "hundreds",
+        "decimals",
+        "tens",
+        "ones",
+        "integers",
+    ]:
+        for i, t in enumerate(match.starts(groups_keys.index(key) + 1)):
+            collection.add_value(key, groups[key][i], t)
 
-    return DurationValue(value)
+    values = []
+    for item in collection.items:
+        numeral = parse_numeral(item)
+        valueunit = get_matched_value(item.text)
+        if numeral:
+            valueunit.value = numeral
+        values.append(valueunit)
+    for item in groups.get("fractions", []):
+        values.append(get_unit_fraction_value(item))
+
+    values = merge_same_units(values)
+    values.sort(key=lambda v: v.unit.value, reverse=True)
+    return DurationValue(values)
 
 
-seconds_group = named_group("seconds", get_pattern("seconds"))
-minutes_group = named_group("minutes", get_pattern("minutes"))
-hours_group = named_group("hours", get_pattern("hours"))
-days_group = named_group("days", get_pattern("days"))
-weeks_group = named_group("weeks", get_pattern("weeks"))
-months_group = named_group("months", get_pattern("months"))
-years_group = named_group("years", get_pattern("years"))
+_seconds = named_group(
+    "seconds", non_capturing_group(ONE_SECOND, TWO_SECONDS, SEVERAL_SECONDS)
+)
+_minutes = named_group(
+    "minutes", non_capturing_group(ONE_MINUTE, TWO_MINUTES, SEVERAL_MINUTES)
+)
+_hours = named_group("hours", non_capturing_group(ONE_HOUR, TWO_HOURS, SEVERAL_HOURS))
+_days = named_group("days", non_capturing_group(ONE_DAY, TWO_DAYS, SEVERAL_DAYS))
+_weeks = named_group("weeks", non_capturing_group(ONE_WEEK, TWO_WEEKS, SEVERAL_WEEKS))
+_months = named_group(
+    "months", non_capturing_group(ONE_MONTH, TWO_MONTHS, SEVERAL_MONTHS)
+)
+_years = named_group("years", non_capturing_group(ONE_YEAR, TWO_YEARS, SEVERAL_YEARS))
 
-RULE_DURATION_SECONDS = FunctionValue(parse_duration, combine_patterns(seconds_group))
-RULE_DURATION_MINUTES = FunctionValue(parse_duration, combine_patterns(minutes_group))
-RULE_DURATION_HOURS = FunctionValue(parse_duration, combine_patterns(hours_group))
-RULE_DURATION_DAYS = FunctionValue(parse_duration, combine_patterns(days_group))
-RULE_DURATION_WEEKS = FunctionValue(parse_duration, combine_patterns(weeks_group))
-RULE_DURATION_MONTHS = FunctionValue(parse_duration, combine_patterns(months_group))
-RULE_DURATION_YEARS = FunctionValue(parse_duration, combine_patterns(years_group))
+all_units = non_capturing_group(
+    _seconds, _minutes, _hours, _days, _weeks, _months, _years
+)
+dual_units = non_capturing_group(
+    named_group("seconds", TWO_SECONDS),
+    named_group("minutes", TWO_MINUTES),
+    named_group("hours", TWO_HOURS),
+    named_group("days", TWO_DAYS),
+    named_group("weeks", TWO_WEEKS),
+    named_group("months", TWO_MONTHS),
+    named_group("years", TWO_YEARS),
+)
+singular_units = non_capturing_group(
+    named_group("seconds", ONE_SECOND),
+    named_group("minutes", ONE_MINUTE),
+    named_group("hours", ONE_HOUR),
+    named_group("days", ONE_DAY),
+    named_group("weeks", ONE_WEEK),
+    named_group("months", ONE_MONTH),
+    named_group("years", ONE_YEAR),
+)
+
+RULE_DURATION_SECONDS = FunctionValue(
+    parse_duration,
+    combine_patterns(
+        get_pattern(
+            ONE_SECOND,
+            named_group("seconds", ONE_SECOND),
+            named_group("seconds", TWO_SECONDS),
+            _seconds,
+        )
+    ),
+)
+RULE_DURATION_MINUTES = FunctionValue(
+    parse_duration,
+    combine_patterns(
+        get_pattern(
+            ONE_MINUTE,
+            named_group("minutes", ONE_MINUTE),
+            named_group("minutes", TWO_MINUTES),
+            _minutes,
+        )
+    ),
+)
+RULE_DURATION_HOURS = FunctionValue(
+    parse_duration,
+    combine_patterns(
+        get_pattern(
+            ONE_HOUR,
+            named_group("hours", ONE_HOUR),
+            named_group("hours", TWO_HOURS),
+            _hours,
+        )
+    ),
+)
+RULE_DURATION_DAYS = FunctionValue(
+    parse_duration,
+    combine_patterns(
+        get_pattern(
+            ONE_DAY,
+            named_group("days", ONE_DAY),
+            named_group("days", TWO_DAYS),
+            _days,
+        )
+    ),
+)
+RULE_DURATION_WEEKS = FunctionValue(
+    parse_duration,
+    combine_patterns(
+        get_pattern(
+            ONE_WEEK,
+            named_group("weeks", ONE_WEEK),
+            named_group("weeks", TWO_WEEKS),
+            _weeks,
+        )
+    ),
+)
+RULE_DURATION_MONTHS = FunctionValue(
+    parse_duration,
+    combine_patterns(
+        get_pattern(
+            ONE_MONTH,
+            named_group("months", ONE_MONTH),
+            named_group("months", TWO_MONTHS),
+            _months,
+        )
+    ),
+)
+RULE_DURATION_YEARS = FunctionValue(
+    parse_duration,
+    combine_patterns(
+        get_pattern(
+            ONE_YEAR,
+            named_group("years", ONE_YEAR),
+            named_group("years", TWO_YEARS),
+            _years,
+        )
+    ),
+)
 
 
 RULE_DURATION = FunctionValue(
     parse_duration,
     combine_patterns(
-        years_group,
-        months_group,
-        weeks_group,
-        days_group,
-        hours_group,
-        minutes_group,
-        seconds_group,
+        get_pattern(
+            non_capturing_group(
+                ONE_SECOND, ONE_MINUTE, ONE_HOUR, ONE_DAY, ONE_WEEK, ONE_MONTH, ONE_YEAR
+            ),
+            singular_units,
+            dual_units,
+            all_units,
+        ),
         combine_all=True,
     ),
 )
