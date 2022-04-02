f5ee019e9ea35f6ea75515526cef2b88f7918be6
Mohammad Al-Fetyani <m4bh@hotmail.com>
2022-04-02 23:29:00 +0300

    Test both to number and to text

--- tests/cleaners/test_num2text.py
+++ tests/cleaners/test_num2text.py
@@ -8,7 +8,7 @@ from maha.parsers.utils import convert_to_number_if_possible
 def assert_both_ways(input, expected, accusative=False):
     number = convert_to_number_if_possible(input)
     assert numbers_to_text(input, accusative) == expected
-    # assert parse_dimension(expected, numeral=True)[0].value == number
+    assert parse_dimension(expected, numeral=True)[0].value == number
 
 
 @pytest.mark.parametrize(
@@ -542,11 +542,11 @@ def test_trillions_and_more(input, expected):
     "input, expected",
     [
         ("50.0", "خمسون"),
+        ("50.0000", "خمسون"),
         ("50.1", "خمسون فاصلة واحد"),
         ("1.20", "واحد فاصلة إثنان"),
         ("00001.200000", "واحد فاصلة إثنان"),
         ("10001.200001", "عشرة آلاف وواحد فاصلة مئتان ألف وواحد"),
-        ("1.01", "واحد فاصلة واحد من مائة"),
         ("1.001", "واحد فاصلة واحد من ألف"),
         ("1.0001", "واحد فاصلة واحد من عشرة آلاف"),
         ("1.00001", "واحد فاصلة واحد من مائة ألف"),
